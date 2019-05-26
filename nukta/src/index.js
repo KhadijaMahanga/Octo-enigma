c4151ef5c831b95a701ec7376d41df61a1e3e218
khadija <khadijamahanga@y7mail.com>
2019-05-26 10:37:21 +0300

    rearraged assets

--- nukta/src/index.js
+++ nukta/src/index.js
@@ -2,9 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './ionicons.css';
-import './bootstrap.css';
-import './style.css';
+import './assets/css/ionicons.css';
+import './assets/css/bootstrap.css';
+import './assets/css/style.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
