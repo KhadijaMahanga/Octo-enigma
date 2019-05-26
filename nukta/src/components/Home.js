700dd3ef563f9e94ca438835e2d57bbbe8ce95d7
khadija <khadijamahanga@y7mail.com>
2019-05-26 14:01:44 +0300

    enhance menu item to use api

--- nukta/src/components/Home.js
+++ nukta/src/components/Home.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import Homeshow from './Homeshow.js';
-import Title from './Title.js';
-import Cardlow from './Cardlow.js';
-import Card from './Card.js';
-import Subscribe from './Subscribe.js'
+import Homeshow from './Homeshow';
+import Title from './Title';
+import Cardlow from './Cardlow';
+import Card from './Card';
+import Subscribe from './Subscribe';
+import Layout from './Layout';
 
 class Home extends Component {
 	constructor(props) {
     	super(props);
     	// This binding is necessary to make `this` work in the callback
-    	this.moreNews = this.moreNews.bind(this);
+    	//this.moreNews = this.moreNews.bind(this);
   	}
 	moreNews(e){
 		e.preventDefault();
@@ -19,29 +20,30 @@ class Home extends Component {
 				console.log(obj);
 				console.log(result);
 				obj.state.data.news=obj.state.data.news.concat(result.news);
-				obj.state.data.ids=result.ids;		
+				obj.state.data.ids=result.ids;
 				obj.setState({data:obj.state.data});
 			},this.props.parent)
 	}
-	
+
 	render() {
-		const newsList = this.props.parentState.data.news?this.props.parentState.data.news.map((row,index,)=>{
-																				return(
-																					   <Card key={index} cardClass="col-sm-4" cardInfo={row}/>
-																					)
-																				}):null;
-		const popularList = this.props.parentState.data.popular?this.props.parentState.data.popular.map((row,index,)=>{
-																				if(index<5)
-																					return(
-																					   <Cardlow key={index} cardClass="oflow-hidden pos-relative mb-20 dplay-block" cardInfo={row}/>
-																					)
-																				else return(<div key={index}></div>);
-																			}):null;
+		// const newsList = this.props.parentState.data.news?this.props.parentState.data.news.map((row,index,)=>{
+		// 																		return(
+		// 																			   <Card key={index} cardClass="col-sm-4" cardInfo={row}/>
+		// 																			)
+		// 																		}):null;
+		// const popularList = this.props.parentState.data.popular?this.props.parentState.data.popular.map((row,index,)=>{
+		// 																		if(index<5)
+		// 																			return(
+		// 																			   <Cardlow key={index} cardClass="oflow-hidden pos-relative mb-20 dplay-block" cardInfo={row}/>
+		// 																			)
+		// 																		else return(<div key={index}></div>);
+		// 																	}):null;
 		return (
-			<div>
-			  <Homeshow data={this.props.parentState.data.recent}/>
+			<Layout>
+				<div>Homepage</div>
+			  {/* <Homeshow data={this.props.parentState.data.recent}/>
 			  <div className="section pv-50 text-left">
-				<div className="container">  
+				<div className="container">
 					<div className="row">
 						<div className="col-md-12 col-lg-8">
 							<Title name="HABARI MPYA"/>
@@ -66,7 +68,7 @@ class Home extends Component {
 							<div className="pl-20 pl-md-0">
 								<div className="mb-50">
 									<Title name="ZILIZOSOMWA ZAIDI"/>
-									{popularList}									
+									{popularList}
 								</div>
 								<Subscribe post={this.props.post} url={this.props.url} />
 							</div>
@@ -78,8 +80,8 @@ class Home extends Component {
 						<a className="dplay-block btn-brdr-primary mt-20 mb-md-50" onClick={this.moreNews} href=""><b>PATA HABARI ZAIDI TOKA NUKTA</b></a>
 					</div>
 				</div>
-			</div>
-		</div>
+			</div> */}
+		</Layout>
 		);
 	}
 }
--- nukta/src/components/Menu.js
+++ nukta/src/components/Menu.js
@@ -1,39 +1,62 @@
 import $ from 'jquery'
 import React, { Component } from 'react';
-import {withRouter,Link} from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import logo from '../assets/images/logos/logo.svg';
 
 class Menu extends Component {
 	constructor(props){
 		super(props);
+
+		this.state = {
+				menus: []
+		}
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	componentDidMount(){
-		var srcBtn = $('.src-btn');
-		var srcIcn = $('.src-icn');
-		var closeIcn = $('.close-icn');
-		var srcForm = $('.src-form');
-		var mnItm = $('.menu-item');
-		srcBtn.on('click', function(){
-			if($(".question").val()!=="")
+	componentDidMount() {
+		fetch('http://localhost:8000/wp-json/menus/v1/menus/nukta-menu')
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				const menus = [];
+				if(data.items.length > 0) {
+					data.items.map(item => {
+						menus.push({
+							"name": item.title,
+							"slug": item.slug,
+							"order": item.menu_order
+						});
+					});
+				}
+				this.setState({
+					menus: menus
+				});
+			});
+			var srcBtn = $('.src-btn');
+			var srcIcn = $('.src-icn');
+			var closeIcn = $('.close-icn');
+			var srcForm = $('.src-form');
+			var mnItm = $('.menu-item');
+			srcBtn.on('click', function(){
+				if($(".question").val()!=="")
 				$(".question").val("")
-			else{
-				$(srcIcn).toggleClass('active');
-				$(closeIcn).toggleClass('active');
-				$(srcForm).toggleClass('active');
-			}
-		});
+				else{
+					$(srcIcn).toggleClass('active');
+					$(closeIcn).toggleClass('active');
+					$(srcForm).toggleClass('active');
+				}
+			});
 
-		$('[data-menu]').on('click', function(){
-			var mainMenu = $(this).data('menu');
-			$(mainMenu).toggleClass('visible-menu');
-		});
+			$('[data-menu]').on('click', function(){
+				var mainMenu = $(this).data('menu');
+				$(mainMenu).toggleClass('visible-menu');
+			});
 
-		mnItm.on('click', function(){
-			console.log("click");
-		});
+			mnItm.on('click', function(){
+				console.log("click");
+			});
 	}
+
 
 	toggleMenu(){
 		$('#main-menu').toggleClass('visible-menu');
@@ -46,33 +69,35 @@ class Menu extends Component {
 	}
 
 	render() {
-		const items = this.props.menuItems==null?null:this.props.menuItems.map((row,index)=>{
-						return(
-							<li menu-item="menu-item" className="menu-item" key={index} onClick={this.toggleMenu}><Link to={'/category/'+row.name}>{row.name}</Link></li>
-						);
-					});
+		const { menus, searchOpen } = this.state;
 
-    	return (
-    		<div className="container">
-			<Link to="/" className="logo"><img src={logo} alt="Logo"/></Link>
+  	return (
+  		<div className="container">
+				<Link to="/" className="logo"><img src={logo} alt="Logo"/></Link>
 
-			<a className="right-area src-btn">
-				<i className="active src-icn ion-search"></i>
-				<i className="close-icn ion-close"></i>
-			</a>
-			<div className="src-form">
-				<form onSubmit={this.handleSubmit}>
-					<input type="text" className="question" placeholder="Search here"/>
-					<button type="submit"><i className="ion-search"></i></button>
-				</form>
+				<a className="right-area src-btn" onClick={this.handleSearchClick}>
+					<i className="close-icn ion-close"></i>
+					<i className="active src-icn ion-search"></i>
+				</a>
+
+				<div className="src-form">
+					<form onSubmit={this.handleSubmit}>
+						<input type="text" className="question" placeholder="Search here"/>
+						<button type="submit"><i className="ion-search"></i></button>
+					</form>
+				</div>
+
+				<a className="menu-nav-icon" data-menu="#main-menu"><i className="ion-navicon"></i></a>
+				<ul className="main-menu" id="main-menu">
+					{menus.map(menu => (
+						<li menu-item="menu-item" className="menu-item" key={menu.order} onClick={this.toggleMenu}>
+							<Link to={`/${menu.slug}`}>{menu.name}</Link>
+						</li>
+					))}
+				</ul>
+				<div className="clearfix"></div>
 			</div>
-			<a className="menu-nav-icon" data-menu="#main-menu"><i className="ion-navicon"></i></a>
-			<ul className="main-menu" id="main-menu">
-				{items}
-			</ul>
-			<div className="clearfix"></div>
-		</div>
-    );
+  	);
   }
 }
 
