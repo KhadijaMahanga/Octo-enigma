943772cdd9d3d2662e3ebb053a576c6f1e91dd63
khadija <khadijamahanga@y7mail.com>
2019-06-16 19:53:18 +0300

    popularlist component

--- nukta/src/components/PopularList.js
+++ nukta/src/components/PopularList.js
@@ -1,11 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import Cardlow from './Cardlow.js';
+import axios from 'axios';
 
-function PopularList () {
-  return (
-    <Fragment>
+class PopularList extends Component {
+  state = {
+    popularPosts: {},
+    isLoaded: false
+  }
 
-    </Fragment>
+  componentDidMount() {
+    axios.get('https://api.nukta.co.tz/wp-json/wordpress-popular-posts/v1/popular-posts')
+      .then(res => this.setState({
+        popularPosts: res.data,
+        isLoaded: true
+      }))
+      .catch(err => console.log(err));
+  }
 
-  );
+  render() {
+    const { popularPosts, isLoaded } = this.state;
+    if (isLoaded) {
+      return (
+        <Fragment>
+          { popularPosts.length > 0 && .map((post, index) => (
+            <Cardlow key={index} cardclassName="oflow-hidden pos-relative mb-20 dplay-block" cardInfo={post}/>
+          ))}
+        </Fragment>
+
+      );
+    }
+    return null;
+  }
 }
+
+export default PopularList;
